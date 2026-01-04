47c93f8a2656d28e4c27323cb1366e20557f8d33
Mrunal Thamake <mrunalthamake9@gmail.com>
2026-01-04 21:50:00 +0530

    uploaded my remeaning assignments

--- Digital-Clock/script.js
+++ Digital-Clock/script.js
@@ -1,34 +1,48 @@
 var timerText = document.querySelector("#timerText");
 var statusText = document.querySelector("#statusText");
 var dateText = document.querySelector("#dateText");
+var progressBar = document.querySelector("#progressBar");
+var msgText = document.querySelector("#msgText");
+
+const sysMessages = ["SYSTEM ONLINE", "SYNCING...", "SECURE", "OPTIMAL", "CONNECTED"];
 
 function digitalClock() {
+    let dateObj = new Date();
+    
+    let hr = dateObj.getHours();
+    let min = dateObj.getMinutes();
+    let sec = dateObj.getSeconds();
+    
+    let amPm = hr < 12 ? "AM" : "PM";
+    
+    let displayHr = hr % 12 || 12; 
 
-    setInterval(() => {
+    displayHr = displayHr < 10 ? `0${displayHr}` : displayHr;
+    min = min < 10 ? `0${min}` : min;
+    sec = sec < 10 ? `0${sec}` : sec;
 
-        let dateObj = new Date(); 
-        let hr = dateObj.getHours();
-        let min = dateObj.getMinutes();
-        let sec = dateObj.getSeconds();
+    timerText.innerHTML = `${displayHr} : ${min} : ${sec}`;
+    statusText.innerHTML = amPm;
 
-        hr = hr < 10 ? `0${hr}` : hr;
-        min = min < 10 ? `0${min}` : min;
-        sec = sec < 10 ? `0${sec} `: sec;
+    const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+    dateText.innerHTML = dateObj.toLocaleDateString('en-US', options).toUpperCase();
 
-        timerText.innerHTML = `${hr} : ${min} : ${sec}`;
-        statusText.innerHTML = hr < 12 ? "AM" : "PM";
+    let progressPercentage = (dateObj.getSeconds() / 60) * 100;
+    progressBar.style.width = `${progressPercentage}%`;
 
-       
-        let day = dateObj.getDate();
-        let month = dateObj.getMonth() + 1;
-        let year = dateObj.getFullYear();
-
-        day = day < 10 ? `0${day}` : day;
-        month = month < 10 ? `0${month}` : month;
-        year = year < 10 ? `0${year}` : year;
-
-        dateText.innerHTML = `${day} / ${month} / ${year}`;
-    }, 1000);
+    if (progressPercentage > 85) {
+        progressBar.style.background = "#ff0055";
+        progressBar.style.boxShadow = "0 0 15px #ff0055";
+    } else {
+        progressBar.style.background = "#00f260";
+        progressBar.style.boxShadow = "0 0 15px #00f260";
+    }
 }
 
-digitalClock()
+setInterval(() => {
+    let randomMsg = sysMessages[Math.floor(Math.random() * sysMessages.length)];
+    msgText.innerText = randomMsg;
+}, 5000);
+
+digitalClock();
+setInterval(digitalClock, 1000);
