50e3ccd6fc50172ce26e6d8acd04598c4a0f0f24
Mrunal Thamake <mrunalthamake9@gmail.com>
2026-04-27 22:19:04 +0530

    last changes of url

--- mongodb-projects/basic-day1/utils/db.js
+++ mongodb-projects/basic-day1/utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://fullstack:fullstack123@fullstack.uock5ec.mongodb.net/')
+        await mongoose.connect('mongodb+srv://fullstack:UJ32w2wCAbIZSI64@fullstack.uock5ec.mongodb.net/')
         console.log('Database connected successfully!!!')
     } catch (error) {
         console.error('Database connection failed!!!')
